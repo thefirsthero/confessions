8dd7df040307554091c1580dd67ec0e8240b8f46
Vukosi <vukosi90@gmail.com>
2023-10-05 10:55:24 +0200

    backend changes

---
+++ backend/README.md
@@ -0,0 +1,45 @@
+# firebase_fastAPI 🚀
+Before you start, I recommend working with virtual environments like:
+- [VENV](https://docs.python.org/3/library/venv.html)
+## REST API with FastAPI (Python) to Firebase Firestore (Google) 🌎
+```
+git clone https://github.com/rtobart/firebase_fastAPI.git
+```
+
+### First, install the necessary dependencies 🖥
+```
+pip install -r requirement.txt
+```
+
+### To use this REST API, you need to download Firebase credentials from your Firebase console 🤓
+To do this process:
+
+- Go to Firebase 🌎
+- Create a new project 🚀
+- Create a Firestore database in production or test mode
+- In the project, select > Project Settings > Service accounts > python 🐍
+- Select "Generate new private key" 🔑
+- Add the generated file to the root of the REST API 📩
+- Rename the file to "serviceAccountKey.json" 📄
+
+
+With this, you can now run your REST API using the command 🖥
+```
+uvicorn main:app --reload 
+```
+## End-points
+
+The URL http://127.0.0.1:8000/docs will render the interactive documentation of FastAPI, where you can test the endpoints you create, as well as the ones already added by default.
+
+The URL http://127.0.0.1:8000/addUser allows you to add users using the POST method. The user data structure is defined in the 'models.py' file at the root of the API.
+
+The URL http://127.0.0.1:8000/ will return the complete list of registered users.
+
+The URL http://127.0.0.1:8000/delUser allows you to delete users using the DELETE method. It requires the user's ID to delete.
+
+The URL http://127.0.0.1:8000/modName allows you to modify the user's name. It requires the user's ID.
+
+For more information, please check the official documentation of these technologies:
+
+- [Firebase documentation](https://firebase.google.com/docs?authuser=0&hl=es)
+- [FastAPI documentation](https://fastapi.tiangolo.com/tutorial/)
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,60 @@
-from typing import Union
+from backend.connection import db
+from fastapi import FastAPI, HTTPException
+from models import User
 
-from fastapi import FastAPI
+# llamamos a la coleccion users de la db(instancia de firestore)
+users = db.collection(u'users')
 
+# creamos una instancia de FastAPI para tener acceso a las rutas 
 app = FastAPI()
 
-
+# definimos el comportamiento de la ruta http://127.0.0.1:8000/ con metodo GET
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    # con users.get() que pertenece a firebase, traemos todos los usuarios de la lista
+    usersRef = users.get()
+    # creamo un diccionario para retornarlo como json
+    usersJson = {}
+    # recorremos la lista de usuarios
+    for user in usersRef:
+        # agregamos cada usuario recuperado al diccionario
+        usersJson[user.id] = user.to_dict()
+    # Retornamos el diccionario
+    return usersJson
+
+# definimos el comportamiento de la ruta http://127.0.0.1:8000/addUser con metodo POST
+@app.post("/addUser")
+async def addUser(user: User): #pasamos por parametro el modelo de usuario {name, lastName, born}
+    # creamos una instancia de la lista de usuarios y añadimos el id como llave del diccionario
+    userAdd = users.document(f'{user.id}') 
+    # agregamos los parametros internos del diccionario y enviamos los cambios
+    userAdd.set({
+        # a la izquierda vemos el nombre del campo en la base de datos
+        u'nombre': user.name, # a la derecha vemos el dato que le pasamos por parametro del POST
+        u'apellido': user.lastName,
+        u'nacimiento': user.born
+    })
+    return {'status' : 200} #retornamos una mensaje de exito
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+# definimos el comportamiento de la ruta http://127.0.0.1:8000/delUser con metodo DELETE
+@app.delete("/delUser")
+async def delUser(id): #pasamos por parametro id de usuario 
+    # se elimina usuario en coleccion que posea id
+    userRef=db.collection(u'users').document(id)
+    user= userRef.get()
+    if user.exists:        
+        users.document(f'{id}').delete()      
+    else:
+        raise HTTPException(status_code=404, detail="User not found") # levantamos excepcion en caso de no existir user con id provisto 
+        
+    return {'status' : 200} #retornamos una mensaje de exito
+
+@app.put("/modName")
+async def modUser(id, name): 
+    userMod = users.document(id)
+    userMod.set({
+        u'nombre': name
+    })
+    return {'status' : 200} 
