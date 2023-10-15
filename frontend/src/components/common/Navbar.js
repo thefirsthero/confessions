bb1994585865500729154222c8f9d0ed4a6efbfe
Vukosi <vukosi90@gmail.com>
2023-10-15 22:29:09 +0200

    making navbar automatically collapse after redirect

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import { NavLink } from 'react-router-dom'; // Import NavLink for routing
+import { NavLink } from 'react-router-dom';
 
 function AppNavbar() {
+  const [expanded, setExpanded] = useState(false);
+
+  const closeNav = () => {
+    setExpanded(false);
+  };
+
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar expand="lg" bg="dark" variant="dark" expanded={expanded}>
       <Container>
         <Navbar.Brand href="#home">Welcome to Confessions</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(!expanded)} />
         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
           <Nav className="ml-auto">
             <Nav.Item>
-              <NavLink to="/make-confession" className="nav-link">
+              <NavLink to="/make-confession" className="nav-link" onClick={closeNav}>
                 Make Confession
               </NavLink>
             </Nav.Item>
             <Nav.Item>
-              <NavLink to="/feed" className="nav-link">
+              <NavLink to="/feed" className="nav-link" onClick={closeNav}>
                 Feed
               </NavLink>
             </Nav.Item>
